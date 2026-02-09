0965efcb175daaeb73a0c318f2888a4fac4b92ff
jthora <jordan.trana@gmail.com>
2026-02-09 13:54:50 -0800

    Switch announcements tag to underscore

--- src/app/announcements.ts
+++ src/app/announcements.ts
@@ -1,7 +1,7 @@
 import {toHex} from "src/util/nostr"
 
 export const ANNOUNCEMENTS_PATH = "/announcements"
-export const ANNOUNCEMENTS_TAG = "starcom-announcements"
+export const ANNOUNCEMENTS_TAG = "starcom_announcements"
 export const ANNOUNCEMENTS_NPUB = "npub1n4ch3caz4w7gj3aswmvzmlj89grp390mnuu7yerlqecqx8999lmspgl00g"
 
 const announcementsPubkey = toHex(ANNOUNCEMENTS_NPUB)
